e1ee08bc62dc38122ca97edd662c1afb39865826
Your Name <your_email@example.com>
2023-05-11 21:14:09 +0200

    homepage: slide images with text

--- app/script.js
+++ app/script.js
@@ -3,6 +3,35 @@ let searchByTitleInput = document.getElementById("searchByTitle");
 let searchBtn = document.getElementById("searchBtn");
 let bookList = document.getElementById("bookList");
 
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slideImages");
+  let dots = document.getElementsByClassName("items");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
 searchBtn.addEventListener("click", searchBooks);
 
 function searchBooks() {
