092270a72862d1bdc0ebd44acd7d8dd43348218f
Nina Trajchevska <nina.ristoska@gmail.com>
2023-05-16 11:30:17 +0200

    changes in detail page

--- app/details/details.js
+++ app/details/details.js
@@ -27,61 +27,13 @@ function showSlides(n) {
 }
 
 
-//Search element
-
-let searchByAuthorInput = document.getElementById("searchByAuthor");
-let searchByTitleInput = document.getElementById("searchByTitle");
-let searchBtn = document.getElementById("searchBtn");
-let bookList = document.getElementById("bookList");
-
-searchBtn.addEventListener("click", searchBooks);
-
-function searchBooks() {
-  let title = searchByTitleInput.value.trim();
-  let author = searchByAuthorInput.value.trim();
-
-  let url = `https://www.googleapis.com/books/v1/volumes?q=`;
-  if (title) {
-    url += `intitle:${title}&`;
-  }
-  if (author) {
-    url += `inauthor:${author}&`;
-  }
-  url += `orderBy=relevance&printType=books&maxResults=40&filter=partial&fields=items(id,volumeInfo/title,volumeInfo/authors,volumeInfo/imageLinks/thumbnail,volumeInfo/categories,volumeInfo/publishedDate,volumeInfo/description)`;
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => displayBooks(data.items))
-    .catch((error) => console.error(error));
-}
-
-//function for displaying the books after the search
-function displayBooks(books) {
-  bookList.innerHTML = "";
-  if (books) {
-    books.forEach((book) => {
-      let li = document.createElement("li");
-      li.innerHTML = `<img src="${book.volumeInfo.imageLinks?.thumbnail}" alt="${book.volumeInfo.title}">
-        <div>
-          <h2>${book.volumeInfo.title}</h2>
-          <p>Author(s): ${book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "Unknown"}</p>
-          <p>Category: ${book.volumeInfo.categories ? book.volumeInfo.categories.join(", ") : "Unknown"}</p>
-          <p>Published Date: ${book.volumeInfo.publishedDate ? book.volumeInfo.publishedDate : "Unknown"}</p>
-          <p>Description: ${book.volumeInfo.description ? book.volumeInfo.description : "N/A"}</p>
-        </div>`;
-      bookList.appendChild(li);
-    });
-  } else {
-    let li = document.createElement("li");
-    li.textContent = "No books found.";
-    bookList.appendChild(li);
-  }
-}
 
 //details
-let titleDesc=document.getElementById("titleDesc");
 let coverImg=document.getElementById("coverImg");
+let titleDesc=document.getElementById("title");
 let categoriesSpan=document.getElementById("categoriesSpan");
+
+
 
 
 function descriptionFunc() {
@@ -102,10 +54,18 @@ function descriptionFunc() {
 function displayDesc(book) {
   if (book) {
   
+    let categoryTable=document.getElementById("categoryTable");
+    let authorTable=document.getElementById("authorTable");
+    let publishedTable=document.getElementById("publishedTable");
+    let desc = document.getElementById("desc");
 
     titleDesc.innerHTML=book.volumeInfo.title;
-    coverImg.innerHTML = `<img src="${book.volumeInfo.imageLinks.thumbnail}" width="70%" height="100%" alt="Cover Image">`;
-    categoriesSpan.innerHTML += book.volumeInfo.categories;
+    coverImg.innerHTML = `<img src="${book.volumeInfo.imageLinks.thumbnail}  alt="Cover Image">`;
+
+    categoryTable.innerHTML+=`<td>  ${book.volumeInfo.categories}</td>`;
+    authorTable.innerHTML+=`<td>  ${book.volumeInfo.authors}</td>`;
+    publishedTable.innerHTML+=`<td>  ${book.volumeInfo.publishedDate}</td>`;
+    desc.innerHTML=`${book.volumeInfo.description}`;
     
   } else {
     console.log("No book found.");
