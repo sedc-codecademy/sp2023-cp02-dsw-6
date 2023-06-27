cbbdfd1e58eedcbc07a8bf4331009d3d300af6c7
Fatima Qazimi <fatima.qazimi@dobi.mk>
2023-06-27 14:57:35 +0200

    vouchers

---
+++ app/vouchers/vouchers.js
@@ -0,0 +1,9 @@
+
+
+let buyButton = document.getElementById(`buyButton`);
+buyButton.addEventListener('click', () => {
+    let select = document.getElementById("select");
+    let selectedValue = select.value;
+    console(selectedValue)
+    let photo =document.getElementById("img")
+})
