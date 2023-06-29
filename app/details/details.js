950795da4235b9325edd5ae40b1808d8ed9d918f
Nina Trajchevska <nina.ristoska@gmail.com>
2023-06-29 09:21:56 +0200

    changes in offerbox and table

--- app/details/details.js
+++ app/details/details.js
@@ -7,19 +7,7 @@ window.addEventListener('load', () => {
     <div id="coverImg"><img id="coverImgBook" src="${bookData.volumeInfo.imageLinks.thumbnail}" alt="Cover Image"></div>
     <div id="textDetails">
       <h1 id="title">${bookData.volumeInfo.title}</h1>
-      <div class="offerBox">
-        <div class="price">${bookData.price}</div>              
-              
-        <div id="detailsButtonAndBuyButtonCart">
-          <button class="detailsButton" id="detailsButtonCart"><i class="fa-solid fa-heart" style="color: #0C54C0;"></i> Wish list </button>
-          <button class="buyButton" id="buyButtonCart"><i class="fa-solid fa-cart-shopping" style="color: #0C54C0;"></i> Buy</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="bookDetails">
-    <div class="bookDetailsTable">
-      <h1 id="bookDet">Book Details</h1>
+     
       <table id="tableDetails">
         <tbody>
           <tr>
@@ -36,6 +24,19 @@ window.addEventListener('load', () => {
           </tr>
         </tbody>
       </table>
+    </div>
+  </div>
+  <div class="bookDetails">
+  <div class="offerBox">
+        <div class="price">${bookData.price}</div>              
+              
+        <div id="detailsButtonAndBuyButtonCart">
+          <button class="detailsButton" id="detailsButtonCart"><i class="fa-solid fa-heart" style="color: #0C54C0;"></i> Wish list </button>
+          <button class="buyButton" id="buyButtonCart"><i class="fa-solid fa-cart-shopping" style="color: #0C54C0;"></i> Buy</button>
+        </div>
+      </div>
+    <div class="bookDetailsTable">
+      
       <div class="descriptionDetails">
         <h1 id="descDetails">Description</h1>
         <p class="desc">${bookData.volumeInfo.description}</p>
