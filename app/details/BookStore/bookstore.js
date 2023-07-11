94ff8f9b5f7caec6e6d68b6e70171de5b1f46eb6
Nina Trajchevska <nina.ristoska@gmail.com>
2023-07-11 12:57:39 +0200

    changes

--- app/details/BookStore/bookstore.js
+++ app/details/BookStore/bookstore.js
@@ -292,17 +292,7 @@ window.location.replace('../../ShoppingCard/shoppingCard.html');
     });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menu");
 
-  hamburger.addEventListener("click", mobileMenu);
-
-  function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  }
-});
 
 
 
